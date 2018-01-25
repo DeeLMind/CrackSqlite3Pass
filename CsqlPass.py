0a67101884b9bb31b10157a16d0db1a01e24cd7b
DeeLMind <DeeLMind@gmail.com>
2018-01-25 10:59:04 +0800

    ADD

--- CsqlPass.py
+++ CsqlPass.py
@@ -1,8 +1,83 @@
-from pysqlcipher3 import dbapi2 as sqlite
-conn = sqlite.connect('test.db')
-c = conn.cursor()
-c.execute("PRAGMA key='password'")
-c.execute('''create table stocks (date text, trans text, symbol text, qty real, price real)''')
-c.execute("""insert into stocks values ('2006-01-05','BUY','RHAT',100,35.14)""")
-conn.commit()
-c.close()
+# # CrackSqlite3Pass
+#To Crack Password of Sqlite3-DB
+
+# NullPass.db has no password to protect but PassDB.db
+
+import os
+import sys
+import sqlite3
+
+class CsqPass:
+    def __banner(self):
+        print("""
+CsqPass:
+        CrackSqlite3Pass
+        By DeeLMind
+        Github  :https://github.com/DeeLMind
+        Blog    :http://www.cnblogs.com/DeeLMind
+        Blog    :https://deelmind.github.io
+        
+        Usege:
+                CsqPass.py  crack.db   pass.txt
+        """)
+
+    __PassFile = []
+    __passPath = ''
+    __dbPath = ''
+    __sql = 'CREATE TABLE ATTACK(ID INT)'
+
+
+    def __getPara(self):
+        self.__dbPath = sys.argv[1]
+        self.__passPath = sys.argv[2]
+
+    # init with dbPath
+    def __init__(self):
+        self.__banner()
+        self.__getPara()
+        self.__getPass(self.__passPath)
+        if self.__isExist(self.__dbPath):
+            print("Not Exist dbFile")
+            exit(1)
+
+        if self.__isExist(self.__passPath):
+            print("Not Exist passFile")
+            exit(2)
+        pass
+
+    def __isExist(self,fileName):
+        if not os.path.exists(fileName):
+            return True
+        else:
+            return False
+
+    def __readFile(self,fileName):
+
+        with open(fileName,'r') as readFile:
+            return readFile.readlines()
+
+    def __getPass(self,fileName):
+        self.__PassFile = self.__readFile(fileName=fileName)
+        return self.__PassFile
+
+
+    def attack(self):
+        with sqlite3.connect(self.__dbPath) as db:
+            cursor = db.cursor()
+
+
+
+
+
+    def test(self):
+        print(self.__PassFile)
+        print(self.__dbPath)
+        print(self.__passPath)
+
+
+
+if __name__ == '__main__':
+    c = CsqPass()
+    c.test()
+
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CrackSqlite3Pass
 To Crack Password of Sqlite3-DB
+
+# NullPass.db has no password to protect but PassDB.db
